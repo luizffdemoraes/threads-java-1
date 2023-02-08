8cdd9eaa140b23419a0ab284218a051d14007423
Luiz Felipe <lffm1994@gmail.com>
2023-02-07 23:19:59 -0300

    Obtendos locks de forma explícita

--- src/main/java/br/com/alura/treads/aula3/ReentrantLocks.java
+++ src/main/java/br/com/alura/treads/aula3/ReentrantLocks.java
@@ -42,4 +42,19 @@ public class ReentrantLocks {
         System.out.println("saindo do banheiro");
         lock.unlock();
     }
+
+    /*
+    Nesse capítulo vimos o poder do bloco synchronized em como ele nos ajuda a trabalhar com tarefas atômicas.
+    Como alternativa, podemos também conseguir esse bloqueio de forma explícita (programaticamente) através de uma classe chamada ReentrantLock.
+
+    Certamente há algumas diferenças que podemos examinar. Uma das principais é a possibilidade de se criar um lock com timeout usando uma sobrecarga do método tryLock.
+
+    Por exemplo:
+
+    boolean locked = lock.tryLock(5, TimeUnit.SECONDS); //5s
+
+    Com esse método esperamos até cinco segundos e receberemos true caso o lock for obtido.
+    Caso contrário, receberemos false.
+    Como desvantagem, há o fato de o programador ter a responsabilidade de liberar o lock (unlock()).
+     */
 }
